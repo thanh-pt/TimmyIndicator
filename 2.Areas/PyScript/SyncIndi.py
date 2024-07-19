1e1cc96b16341d0860386d8ecc789e79c2efd4c7
ThanhPham <tienthanh.pham@hotmail.com>
2024-07-19 14:15:57 +0700

    update script

--- 2.Areas/PyScript/SyncIndi.py
+++ 2.Areas/PyScript/SyncIndi.py
@@ -6,41 +6,27 @@ targetId = '2191F4A3D14D7B4B1EBB84F924777883'
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 
-source = os.path.abspath(os.path.join(script_path, "../../../../")) + '/'
+source = os.path.abspath(os.path.join(script_path, "../../../../../")) + '/'
 target = os.path.abspath(os.path.join(source, "../"+targetId))      + '/'
 
-def checkPath(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
+def move_files_with_extension(path_a, path_b, extension=".ex4"):
+    for root, dirs, files in os.walk(path_a):
+        # Tính toán thư mục con tương ứng trong path_b
+        relative_path = os.path.relpath(root, path_a)
+        target_dir = os.path.join(path_b, relative_path)
+        
+        # Tạo thư mục tương ứng trong path_b nếu chưa tồn tại
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+        for file in files:
+            if file.endswith(extension):
+                source_file = os.path.join(root, file)
+                target_file = os.path.join(target_dir, file)
+                
+                # Di chuyển file từ path_a sang path_b, ghi đè nếu đã tồn tại
+                shutil.copy2(source_file, target_file)
+                print(f"Đã chuyển: {file}")
 
-def move_files_ext(source_folder, target_folder, extention):
-    # Lặp qua tất cả các file trong thư mục nguồn
-    for filename in os.listdir(source_folder):
-        # Kiểm tra nếu là file có đuôi '.ex4'
-        if filename.endswith(extention):
-            # Đường dẫn đầy đủ tới file nguồn và đích
-            source_file = os.path.join(source_folder, filename)
-            target_file = os.path.join(target_folder, filename)
-            
-            # Kiểm tra nếu thư mục đích không tồn tại, tạo mới
-            if not os.path.exists(target_folder):
-                os.makedirs(target_folder)
-            
-            # Copy file từ nguồn sang đích
-            shutil.copyfile(source_file, target_file)
-            print(f"Đã chuyển file: {filename}")
-
-timmyMkrFile   = 'TimmyMaker.ex4'
-timmyMkrExPath = 'MQL4/Experts/TimmyMaker/'
-timmyMkrIdPath = 'MQL4/Indicators/TimmyMaker_Indi_Def/'
-commonIndiPath = 'MQL4/Indicators/TimmyIndicator/'
-
-checkPath(target+timmyMkrIdPath)
-checkPath(target+timmyMkrExPath)
-checkPath(target+commonIndiPath)
-
-shutil.copyfile(source+timmyMkrIdPath+timmyMkrFile, target+timmyMkrIdPath+timmyMkrFile)
-shutil.copyfile(source+timmyMkrExPath+timmyMkrFile, target+timmyMkrExPath+timmyMkrFile)
-
-move_files_ext(source+commonIndiPath, target+commonIndiPath, '.ex4')
-move_files_ext(source+'templates/'  , target+'templates/'  , '.tpl')
+move_files_with_extension(source+'MQL4/Indicators/', target+'MQL4/Indicators/', ".ex4")
+move_files_with_extension(source+'MQL4/Experts/', target+'MQL4/Experts/', ".ex4")
+move_files_with_extension(source+'templates/', target+'templates/', ".tpl")
