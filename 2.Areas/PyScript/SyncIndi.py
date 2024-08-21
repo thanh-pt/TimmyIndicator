8a2b3a900452d91cbeb13b95c67b96244f1e8318
ThanhPham <tienthanh.pham@hotmail.com>
2024-08-21 22:19:08 +0700

    update

--- 2.Areas/PyScript/SyncIndi.py
+++ 2.Areas/PyScript/SyncIndi.py
@@ -1,12 +1,6 @@
 import shutil
 import os
 
-targetId = '2191F4A3D14D7B4B1EBB84F924777883'
-
-script_path = os.path.dirname(os.path.abspath(__file__))
-
-source = os.path.abspath(os.path.join(script_path, "../../../../../")) + '/'
-target = os.path.abspath(os.path.join(source, "../"+targetId))      + '/'
 
 def move_files_with_extension(path_a, path_b, extension=".ex4"):
     for root, dirs, files in os.walk(path_a):
@@ -26,6 +20,17 @@ def move_files_with_extension(path_a, path_b, extension=".ex4"):
                 shutil.copy2(source_file, target_file)
                 print(f"Đã chuyển: {file}")
 
+script_path = os.path.dirname(os.path.abspath(__file__))
+source = os.path.abspath(os.path.join(script_path, "../../../../../")) + '/'
+
+targetId = '2191F4A3D14D7B4B1EBB84F924777883'
+target = os.path.abspath(os.path.join(source, "../"+targetId))      + '/'
+move_files_with_extension(source+'MQL4/Indicators/', target+'MQL4/Indicators/', ".ex4")
+move_files_with_extension(source+'MQL4/Experts/', target+'MQL4/Experts/', ".ex4")
+move_files_with_extension(source+'templates/', target+'templates/', ".tpl")
+
+targetId = '4B5AFECFE6DF22D551907BBA5F00BB79'
+target = os.path.abspath(os.path.join(source, "../"+targetId))      + '/'
 move_files_with_extension(source+'MQL4/Indicators/', target+'MQL4/Indicators/', ".ex4")
 move_files_with_extension(source+'MQL4/Experts/', target+'MQL4/Experts/', ".ex4")
 move_files_with_extension(source+'templates/', target+'templates/', ".tpl")
